92226968289bb92b2abc9faa29ee59d85a64adbc
Tara <tobia.fjellman@epfl.ch>
2024-04-13 18:30:13 +0200

    added plot of episonde times

--- agents.py
+++ agents.py
@@ -8,23 +8,9 @@ class RandomAgent:
     def observe(self, state, action, next_state, reward):
         pass
 
-    def select_action(self, state):
-        # Select a random action from the available actions
-        return random.choice(state.available_actions)
+    def select_action(self, state, env): # shouldn't this have access to the environment ? -> could then use action = env.action_space.sample()
+        return env.action_space.sample()
+    #    return random.choice(state.available_actions)
 
     def update(self):
         pass
-
-
-done = False
-state, _ = env.reset()
-episode_reward = 0
-
-while not done:
-    action = env.action_space.sample()
-    next_state, reward, terimnated, truncated, _ = env.step(action)
-
-    episode_reward += reward
-
-    state = next_state
-    done = terminated or truncated
--- sift.py
+++ sift.py
@@ -1,8 +1,85 @@
 import gymnasium as gym
+import agents 
+import numpy as np
 
-env = gym.make('MountainCar-v0', render_mode='human')
+env = gym.make('MountainCar-v0', render_mode='rgb_array')
+state, info = env.reset()
+agent = agents.RandomAgent()
 
-observation, info = env.reset()
+
+def run_episode(env, agent):
+    state, info = env.reset()
+    done = False
+    episode_reward = 0
+    count = 0
+    while not done:
+        action = agent.select_action(state,env) 
+        next_state, reward, terminated, truncated, _ = env.step(action)
+        agent.observe(state, action, next_state, reward)
+        agent.update()
+
+        episode_reward += reward
+        state = next_state
+        done = terminated or truncated
+        count += 1
+
+    return count
+
+# run 100 episodes and plot the duration of each in a scatter plot
+durations = [run_episode(env, agent) for _ in range(100)]
+import matplotlib.pyplot as plt
+plt.scatter(range(100), durations)
+plt.show()
+
+
+'''
+done = False
+episode_reward = 0
+states = np.zeros((1000, 2))
+states[0] = state
+count = 0
+images = []
+while not done:
+    action = agent.select_action(state,env) 
+    next_state, reward, terminated, truncated, _ = env.step(action)
+    agent.observe(state, action, next_state, reward)
+    agent.update()
+
+    episode_reward += reward
+    state = next_state
+    states[count+1] = state
+
+    done = terminated or truncated
+    count += 1
+    images.append(env.render())
+env.close()
+
+
+
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+# plot the images with a .1 second delay
+
+fig = plt.figure()
+# updates the data and graph
+def update(frame):
+    # creating a new graph or updating the graph
+    plt.imshow(frame)
+ 
+anim = FuncAnimation(fig, update, frames = images, cache_frame_data=False, interval=50)
+plt.show()
+'''
+
+'''
+fig = plt.figure()
+for i in range(len(images)):
+    plt.imshow(images[i])
+    plt.pause(.1)
+    plt.draw()
+'''
+
+
+'''
 
 for _ in range(2500):
     action = env.action_space.sample()  # agent policy that uses the observation and info
@@ -12,22 +89,6 @@ for _ in range(2500):
         observation, info = env.reset()
 
 env.close()
-
-'''
-done = False
-state, _ = env.reset()
-episode_reward = 0
-states = np.zeros((1000, 2))
-states[0] = state
-while not done:
-    action = env.action_space.sample()
-    next_state, reward, terimnated, truncated, _ = env.step(action)
-
-    episode_reward += reward
-
-    state = next_state
-    states[env.current_step] = state
-    done = terminated or truncated
 
 '''
 
