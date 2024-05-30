2112dc37b79000d32d2b4343c8fb2bff5bcc8402
Tara <tobia.fjellman@epfl.ch>
2024-05-30 11:51:36 +0200

    Removed pip adaptation (packages are different than in conda and it is sufficient to download the gymnasium package on colab for the code to run) + defined new config files in view of the submission.

--- code/utility.py
+++ code/utility.py
@@ -1,7 +1,7 @@
 import random
 import torch 
 import torch.nn as nn
-import yaml
+
 
 class MLP( nn.Module ):
     '''
@@ -54,59 +54,3 @@ class ReplayBuffer():
         batch = self.values[batch_ind,:]
         return batch
     
-def conda_to_pip_requirements(yaml_file, requirements_file):
-    with open(yaml_file, 'r') as stream:
-        try:
-            env_data = yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
-            return
-
-    dependencies = env_data.get('dependencies', [])
-    pip_deps = []
-    for dep in dependencies:
-        if isinstance(dep, dict) and 'pip' in dep:
-            pip_deps.extend(dep['pip'])
-        elif isinstance(dep, str):
-            pip_deps.append(dep)
-
-    with open(requirements_file, 'w') as req_file:
-        for dep in pip_deps:
-            req_file.write(dep + '\n')
-
-import json
-import os
-
-
-
-if __name__ == '__main__':
-    #conda_to_pip_requirements('../requirements.yml', '../requirements.txt')
-# Load the conda environment file
-    file_path = '../requirements.yml'
-    with open(file_path, 'r') as file:
-        conda_env = file.read()
-
-    # Parse the dependencies
-    dependencies = conda_env.split('dependencies:')[-1].strip().split('\n')
-    formatted_dependencies = []
-
-    for dep in dependencies:
-
-        if dep.startswith('- ') or dep.startswith('  - '):
-            if dep.startswith('- '):
-                dep = dep[2:].strip()
-            elif dep.startswith('  -'):
-                dep = dep[4:].strip()
-            if '=' in dep:
-                parts = dep.split('=')
-                if len(parts) >= 2:
-                    package_name = parts[0].strip()
-                    package_version = parts[1].strip()
-                    formatted_dependencies.append(f"{package_name}=={package_version}")
-       # print(dep)
-
-    # Write the formatted dependencies to requirements.txt
-    requirements_file_path = '../requirements.txt'
-    with open(requirements_file_path, 'w') as file:
-        for dep in formatted_dependencies:
-            file.write(f"{dep}\n")
